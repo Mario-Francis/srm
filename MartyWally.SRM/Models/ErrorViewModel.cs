a69e62d1798ca0c983cb90b62eea277c5b24cc04
mario-francis <mariofrancis@yahoo.com>
2023-06-01 15:28:01 -0500

    Initial commit

---
+++ MartyWally.SRM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MartyWally.SRM.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
